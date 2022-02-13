efc61a4a147319fe38a9a55f3225d07d89e961a7
Kirill Novichkov <kin190173@gmail.com>
2022-02-13 17:41:08 +0500

    main adaptive done

--- src/components/goods/Goods.js
+++ src/components/goods/Goods.js
@@ -1,32 +1,17 @@
 import Card from "../card/Card"
 
-const CARDS_PER_LINE = 3;
-
 function Goods(props) {
-    const goodCards = props.goods.map((good, index) => 
-        <div className="col-sm-4 py-3 px-3" key={index}>
+    const goods = props.goods.map((good, index) => 
+        <div className="col-sm-12 col-md-6 col-lg-4 py-3 px-3" key={index}>
             <Card good={good} />
         </div>
     )
-    const gridGoods = [];
-    let i = 0;
-    let strGoods = [];
-    while (i< goodCards.length) {
-        for (let j=0; j<CARDS_PER_LINE; j++) {
-            strGoods.push(goodCards[i]);
-            i++;
-        }
-        gridGoods.push(strGoods);
-        strGoods = [];
-    }
-    const goods = gridGoods.map((line, index) =>
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" key={index}>
-            {line}
-        </div>
-    )
-    return(
-        goods
-    )
+
+   return(
+       <div className="row">
+           {goods}
+       </div>
+   )
 }
 
 export default Goods
